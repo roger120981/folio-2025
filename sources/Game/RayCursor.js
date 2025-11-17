5e9b2ab14a3ba6015534d98d9b9932ce995bb3c1
brunosimon <simon.bruno.77@gmail.com>
2025-11-17 10:59:51 +0100

    :bug: Ray Cursor > Test cursor right after adding an intersect

--- sources/Game/RayCursor.js
+++ sources/Game/RayCursor.js
@@ -6,19 +6,16 @@ export class RayCursor
     constructor()
     {
         this.game = Game.getInstance()
+        // console.log(this.game.ticker.wait)
 
         this.currentIntersect = null
         this.intersects = []
         this.isAnyIntersecting = false
+        this.needsTest = false
 
         this.raycaster = new THREE.Raycaster()
 
         this.setPointerTesting()
-
-        // this.game.ticker.events.on('tick', () =>
-        // {
-        //     this.update()
-        // }, 1)
     }
 
     addIntersects(description)
@@ -28,6 +25,19 @@ export class RayCursor
         intersect.isDown = false
         
         this.intersects.push(intersect)
+        this.needsTest = intersect.active
+
+        if(this.needsTest)
+        {
+            this.game.ticker.wait(1, () =>
+            {
+                if(this.needsTest)
+                {
+                    this.testIntersects('change')
+                    this.needsTest = false
+                }
+            })
+        }
 
         return intersect
     }
@@ -37,151 +47,156 @@ export class RayCursor
         this.intersects = this.intersects.filter(_intersect => _intersect !== intersect)
     }
 
+    testIntersects(actionTrigger)
+    {
+        // Start
+        if(actionTrigger === 'start')
+        {
+            this.deltaCursor.x = 0
+            this.deltaCursor.y = 0
+
+            if(this.currentIntersect)
+            {
+                this.currentIntersect.isDown = true
+
+                if(typeof this.currentIntersect.onDown === 'function')
+                    this.currentIntersect.onDown()
+            }
+        }
+
+        // Change
+        else if(actionTrigger === 'change')
+        {
+            const intersects = this.intersects.filter(intersect => intersect.active)
+            let isAnyIntersecting = false
+
+            this.deltaCursor.x += Math.abs(this.game.inputs.pointer.delta.x)
+            this.deltaCursor.y += Math.abs(this.game.inputs.pointer.delta.y)
+
+            if(intersects.length)
+            {
+                const ndcPointer = new THREE.Vector2(
+                    (this.game.inputs.pointer.current.x / this.game.viewport.width) * 2 - 1,
+                    - ((this.game.inputs.pointer.current.y / this.game.viewport.height) * 2 - 1),
+                )
+                this.raycaster.setFromCamera(ndcPointer, this.game.view.camera)
+
+                // Each intersect
+                for(const intersect of intersects)
+                {
+                    let isIntersecting = false
+                    let shapeIndex = 0
+
+                    // Test every shape for intersect
+                    while(!isIntersecting && shapeIndex <= intersect.shapes.length - 1)
+                    {
+                        const shape = intersect.shapes[shapeIndex]
+
+                        if(shape instanceof THREE.Sphere)
+                            isIntersecting = this.raycaster.ray.intersectsSphere(shape)
+                        if(shape instanceof THREE.Box3)
+                            isIntersecting = this.raycaster.ray.intersectsBox(shape)
+                        if(shape instanceof THREE.Plane)
+                            isIntersecting = this.raycaster.ray.intersectsPlane(shape)
+                        if(shape instanceof THREE.Mesh)
+                            isIntersecting = this.raycaster.intersectObject(shape).length
+                        
+                        shapeIndex++
+                    }
+
+                    // Intersect status changed
+                    if(isIntersecting !== intersect.isIntersecting)
+                    {
+                        intersect.isIntersecting = isIntersecting
+
+                        if(intersect.isIntersecting)
+                        {
+                            this.currentIntersect = intersect
+
+                            if(typeof intersect.onEnter === 'function')
+                                intersect.onEnter()
+                        }
+
+                        else
+                        {
+                            if(typeof intersect.onLeave === 'function')
+                                intersect.onLeave()
+                        }
+                    }
+
+                    // Save global intersect status
+                    if(isIntersecting)
+                    {
+                        isAnyIntersecting = true
+                    }
+                }
+
+                // Global intersect status changed
+                if(isAnyIntersecting !== this.isAnyIntersecting)
+                {
+                    this.isAnyIntersecting = isAnyIntersecting
+                    
+                    if(this.isAnyIntersecting)
+                        this.game.canvasElement.style.cursor = 'pointer'
+                    else
+                        this.game.canvasElement.style.cursor = null
+                }
+
+                if(!isAnyIntersecting)
+                    this.currentIntersect = null
+            }
+        }
+
+        // End
+        else if(actionTrigger === 'end')
+        {
+            const intersects = this.intersects.filter(intersect => intersect.active)
+
+            const distance = Math.hypot(this.deltaCursor.x, this.deltaCursor.y)
+            
+            // Each intersect
+            for(const intersect of intersects)
+            {
+                if(intersect.isIntersecting)
+                {
+                    if(typeof intersect.onUp === 'function')
+                    {
+                        intersect.onUp()
+                    }
+                    
+                    if(intersect.isDown)
+                    {
+                        intersect.isDown = false
+
+                        if(typeof intersect.onClick === 'function' && distance < 25)
+                            intersect.onClick()
+                    }
+                }
+                else
+                {
+                    if(intersect.isDown)
+                    {
+                        intersect.isDown = false
+
+                        if(typeof intersect.onUp === 'function')
+                            intersect.onUp()
+                    }
+                }
+            }
+        }
+    }
+
     setPointerTesting()
     {
         this.game.inputs.addActions([
             { name: 'rayPointer', categories: [ 'intro', 'wandering', 'racing', 'cinematic' ], keys: [ 'Pointer.any' ] },
         ])
 
-        const deltaCursor = { x: 0, y: 0 }
+        this.deltaCursor = { x: 0, y: 0 }
 
         this.game.inputs.events.on('rayPointer', (action) =>
         {
-            // Start
-            if(action.trigger === 'start')
-            {
-                deltaCursor.x = 0
-                deltaCursor.y = 0
-
-                if(this.currentIntersect)
-                {
-                    this.currentIntersect.isDown = true
-
-                    if(typeof this.currentIntersect.onDown === 'function')
-                        this.currentIntersect.onDown()
-                }
-            }
-
-            // End
-            else if(action.trigger === 'end')
-            {
-                const intersects = this.intersects.filter(intersect => intersect.active)
-
-                const distance = Math.hypot(deltaCursor.x, deltaCursor.y)
-                
-                // Each intersect
-                for(const intersect of intersects)
-                {
-                    if(intersect.isIntersecting)
-                    {
-                        if(typeof intersect.onUp === 'function')
-                        {
-                            intersect.onUp()
-                        }
-                        
-                        if(intersect.isDown)
-                        {
-                            intersect.isDown = false
-
-                            if(typeof intersect.onClick === 'function' && distance < 25)
-                                intersect.onClick()
-                        }
-                    }
-                    else
-                    {
-                        if(intersect.isDown)
-                        {
-                            intersect.isDown = false
-
-                            if(typeof intersect.onUp === 'function')
-                                intersect.onUp()
-                        }
-                    }
-                }
-            }
-
-            // Change
-            else if(action.trigger === 'change')
-            {
-                const intersects = this.intersects.filter(intersect => intersect.active)
-                let isAnyIntersecting = false
-
-                deltaCursor.x += Math.abs(this.game.inputs.pointer.delta.x)
-                deltaCursor.y += Math.abs(this.game.inputs.pointer.delta.y)
-
-                if(intersects.length)
-                {
-                    const ndcPointer = new THREE.Vector2(
-                        (this.game.inputs.pointer.current.x / this.game.viewport.width) * 2 - 1,
-                        - ((this.game.inputs.pointer.current.y / this.game.viewport.height) * 2 - 1),
-                    )
-                    this.raycaster.setFromCamera(ndcPointer, this.game.view.camera)
-
-                    // Each intersect
-                    for(const intersect of intersects)
-                    {
-                        let isIntersecting = false
-                        let shapeIndex = 0
-
-                        // Each shape of intersect
-                        while(!isIntersecting && shapeIndex <= intersect.shapes.length - 1)
-                        {
-                            const shape = intersect.shapes[shapeIndex]
-
-                            if(shape instanceof THREE.Sphere)
-                                isIntersecting = this.raycaster.ray.intersectsSphere(shape)
-                            if(shape instanceof THREE.Box3)
-                                isIntersecting = this.raycaster.ray.intersectsBox(shape)
-                            if(shape instanceof THREE.Plane)
-                                isIntersecting = this.raycaster.ray.intersectsPlane(shape)
-                            if(shape instanceof THREE.Mesh)
-                                isIntersecting = this.raycaster.intersectObject(shape).length
-                            
-                            shapeIndex++
-                        }
-
-                        // Intersect status changed
-                        if(isIntersecting !== intersect.isIntersecting)
-                        {
-                            intersect.isIntersecting = isIntersecting
-
-                            if(intersect.isIntersecting)
-                            {
-                                this.currentIntersect = intersect
-
-                                if(typeof intersect.onEnter === 'function')
-                                    intersect.onEnter()
-                            }
-
-                            else
-                            {
-                                if(typeof intersect.onLeave === 'function')
-                                    intersect.onLeave()
-                            }
-                        }
-
-                        // Save global intersect status
-                        if(isIntersecting)
-                        {
-                            isAnyIntersecting = true
-                        }
-                    }
-
-                    // Global intersect status changed
-                    if(isAnyIntersecting !== this.isAnyIntersecting)
-                    {
-                        this.isAnyIntersecting = isAnyIntersecting
-                        
-                        if(this.isAnyIntersecting)
-                            this.game.canvasElement.style.cursor = 'pointer'
-                        else
-                            this.game.canvasElement.style.cursor = null
-                    }
-
-                    if(!isAnyIntersecting)
-                        this.currentIntersect = null
-                }
-            }
+            this.testIntersects(action.trigger)
         })
     }
 }
